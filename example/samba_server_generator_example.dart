ea05d612cae9b049d5a06202422848962d0f4b27
tejHackerDev <tejasimha222@gmail.com>
2023-04-17 23:13:58 +0530

    Initial commit
    Signed-off-by: tejHackerDev <tejasimha222@gmail.com>

---
+++ example/samba_server_generator_example.dart
@@ -0,0 +1,6 @@
+import 'package:samba_server_generator/samba_server_generator.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/samba_server_generator.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library samba_server_generator;
+
+export 'src/samba_server_generator_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
